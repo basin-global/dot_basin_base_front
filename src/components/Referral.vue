0695232b64da56b7a7efbd6f84bc5d9540ac723a
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-11-12 20:19:54 +0100

    app.basin.global instead of domains.basin.global

--- src/components/Referral.vue
+++ src/components/Referral.vue
@@ -12,8 +12,8 @@
           <div class="col-md-6 offset-md-3">
             <input 
               class="form-control text-center clipboard"
-              :value="'https://domains.basin.global/?ref=' + this.getNameOrAddress"
-              @click="copyToClipboard('https://domains.basin.gobal/?ref=' + this.getNameOrAddress)"
+              :value="'https://app.basin.global/?ref=' + this.getNameOrAddress"
+              @click="copyToClipboard('https://app.basin.gobal/?ref=' + this.getNameOrAddress)"
               readonly
             >
           </div>
