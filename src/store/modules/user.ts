62e0f3d75c4415bb315555d460ceb9a2cd37b595
TMO <89156835+tmoindustries@users.noreply.github.com>
2024-04-25 00:15:19 -0600

    Update user.ts

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -23,7 +23,7 @@ export default {
     tokenContract: null,
     tokenAllowance: 0, // user's allowance for wrapper contract
     tokenBalance: 0, // user's balance
-    tokenName: "MATIC",
+    tokenName: "ETH",
     tokenDecimals: 18,
     userAddress: null,
     userAllDomainNames: [], // all domain names of current user (default + manually added)
