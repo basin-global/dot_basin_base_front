43329dca0c454e884d11504327cea059178f081e
TMO <89156835+tmoindustries@users.noreply.github.com>
2024-04-24 22:58:17 -0600

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,11 @@
 <template>
 <div class="container">
-  <h1 class="text-center">.basin is $BASIN </h1>
+  <h1 class="text-center">BASIN Accounts</h1>
   
   <div class="row mt-3">
     <div class="col-md-8 offset-md-2">
       <p>
-        basin.global scales real estate restoration and conservation to solve the climate, nature and carbon crises.
-        
-        $BASIN is a vessel of value.  All Tributaries and Confluences (network participants) have a $BASIN.  Every $BASIN is unique.  Any type of value can be stored in a $BASIN to accrue more value and to signal what is truly valuable.
+        BASIN makes Nature investable onchain
       </p>
 
     </div>
